746d9dc903fdc65fe2dc1aa8e39ef4feb9ebede0
MamuteDev <dev.mamute@gmail.com>
2022-08-23 10:18:00 -0300

    primeiro exercicio do bloco 4.

---
+++ Fundamentos/Bloco 4 - Introdução à JavaScript e Lógica de Programação/4.1/exercicio4_1.js
@@ -0,0 +1,16 @@
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = 'Lucas';
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = 'Rio de Janeiro';
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1986;
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName, birthYear, birthCity)
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030;
+console.log(birthYear);
+/* Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente!
+Você saberia explicar por que recebemos uma mensagem de erro? */
+birthCity = London;
+console.log(birthCity);
+//Resposta: A mensagem de erro se dá pela variável ser uma constante e seu valor não pode ser reatribuido.
